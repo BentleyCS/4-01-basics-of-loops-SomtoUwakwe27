29acc4ad1553e26368ee36fe85795fc5345414b8
SomtoUwakwe27 <suwakwe26@bentleyschool.org>
2025-12-11 22:06:36 -0800

    Add files via upload

--- CSP_4_01_Basics_of_Loops.py
+++ CSP_4_01_Basics_of_Loops.py
@@ -1,4 +1,6 @@
 #All questions must use a loop for full points.
+import random
+
 
 def oddNumbers(n:int) ->str:
     """
@@ -7,8 +9,17 @@ def oddNumbers(n:int) ->str:
     example oddNumbers(8) -> "1 3 5 7"
     example oddNumbers(-8) -> ""
     """
-
-
+    answer = ""
+    for odd in range(1,n+1,1):
+        if odd == n:
+            answer += ""
+        elif odd%2 ==0:
+            answer += " "
+        if odd%2 == 1:
+            answer += str(odd)
+    print(answer)
+    return answer
+# oddNumbers(46)
 def backwards(n)-> int:
     """
     modify the below function such that it prints out all the numbers from n to 1
@@ -17,8 +28,15 @@ def backwards(n)-> int:
     example backwards(8) -> "8 7 6 5 4 3 2 1"
     example backwards(-2) -> ""
     """
-
-
+    answer = ""
+    for back in range(n,0,-1):
+        if back > 1:
+            answer += str(back) + " "
+        else:
+            answer += str(back)
+    print(answer)
+    return(answer)
+# backwards(6)
 
 def randomRepeating():
     """
@@ -28,7 +46,13 @@ def randomRepeating():
     :return:
     """
     tries = 0
+    repeat = 0
+    while repeat < 10:
+        repeat = random.randint(1,10)
+        print(repeat, end = " ")
+        tries +=1
     print(f"it took {tries} tries to get a 10")
+# randomRepeating()
 def randomRange(n):
     """
     Generate a random number from 1 to 100 n number of times. Then after that is
@@ -37,13 +61,32 @@ def randomRange(n):
     :param n:
     :return:
     """
+    firstRoll = random.randint(1,100)
+    minNum = firstRoll
+    maxNum = firstRoll
+    print(firstRoll)
+    for high in range(2,n+1):
+        roll = random.randint(1,100)
+        print(roll, end = " ")
+        if roll < minNum:
+            minNum = roll
+        if roll > maxNum:
+            maxNum = roll
+    print("max", maxNum)
+    print("min", minNum)
+#randomRange(5)
 def reverse(word:str)->str:
     """
     Takes in a string as an argument and return the given string in reverse.
     example reverse("cat") -> "tac"
     example reverse("Hello") -> "olleH"
     """
-
+    answer = ""
+    for back in range(len(word)-1, -1, -1):
+        answer += word[back]
+    print(answer)
+    return answer
+#
 def fizzBuzzContinuous(n):
     """
     Modify the function such that it does the fizzbuzz operation on all numbers
@@ -58,6 +101,19 @@ def fizzBuzzContinuous(n):
     :param n:
     :return:
     """
+    ans = ""
+    for i in range(1, n +1, 1):
+        if i % 3==0 and i % 5 == 0:
+            ans += "fizzbuzz" + " "
+        elif i%3 == 0:
+            ans += "fizz" + " "
+        elif i%5 == 0:
+            ans += "buzz" + " "
+        else:
+            ans += str(i) + " "
+
+    return ans[0:-1]
+print(fizzBuzzContinuous(2))
 
 def collatz(n):
     """
@@ -69,8 +125,15 @@ def collatz(n):
     :param n:
     :return:
     """
-
-
+    answer = str(n)+" "
+    while n!=1:
+        if n%2 ==0:
+            n//=2
+        elif n%2 ==1:
+            n= n* 3+1
+        answer+= str(n)+" "
+    return answer[0:-1]
+print(collatz(9))
 def fibonacci(n):
     """
     for the given argument n print out the first n numbers of the fibonacci
@@ -83,6 +146,17 @@ def fibonacci(n):
     :param n:
     :return:
     """
-
-
-print(fibonacci(300))
+    answer = ""
+    first = 0
+    second = 1
+    for prior in range(n):
+        if prior <= 1:
+            answer += str(prior)+" "
+        else:
+            add = first + second
+            answer += str(add) + " "
+            first = second
+            second = add
+    print(answer)
+    return answer[0:-1]
+# print(fibonacci(5))
